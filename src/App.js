fb25540653af35e833d7f46dd0612c3e2cc44e8e
Lucas-PC <lgbs@cesar.school>
2024-06-25 16:05:52 -0300

    create Container component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import Banner from "./components/Banner";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import Container from "./components/Container";
 
 function App() {
   return (
     <div>
       <Header />
       <Banner />
-      <h1>Hello Word!</h1>
+      <Container>
+        <h1>Hello Word!</h1>
+        <p>Dale familia</p>
+      </Container>
       <Footer />
     </div>
   );
