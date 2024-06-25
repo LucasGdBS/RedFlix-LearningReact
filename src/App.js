06959affb1c8ecd523573d92f4457af6c8d04443
Lucas-PC <lgbs@cesar.school>
2024-06-25 17:52:34 -0300

    create card component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import Banner from "./components/Banner";
+import Card from "./components/Card";
+import Container from "./components/Container";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import Container from "./components/Container";
-import bannerHome from "./components/Banner/images/banner-home.png"
-import bannerAssistir from "./components/Banner/images/banner-assistir.png"
-import bannerFavoritos from "./components/Banner/images/banner-favoritos.png"
+import db from "./json/db.json";
+
+import bannerHome from "./components/Banner/images/banner-home.png";
 
 function App() {
   return (
     <div>
       <Header />
-      <Banner image={bannerHome}/>
+      <Banner image={bannerHome} />
       <Container>
-        <h1>Hello Word!</h1>
-        <p>Dale familia</p>
+        <h2>Categoria generica</h2>
+        <section className="cards-container">
+          {db.map((item) => (
+            <Card key={item.id} id={item.id} />
+          ))}
+        </section>
       </Container>
       <Footer />
     </div>
