beecf9f1c85b2604250085ea517493dadd8c468c
Lucas-PC <lgbs@cesar.school>
2024-06-25 20:31:59 -0300

    create pages folder for separate pages and adding react router

--- src/App.js
+++ src/App.js
@@ -1,34 +1,8 @@
-import Banner from "./components/Banner";
-import Card from "./components/Card";
-import Category from "./components/Category";
-import Container from "./components/Container";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import videos from "./json/videos.json";
-
-import bannerHome from "./components/Banner/images/banner-home.png";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div>
-      <Header />
-      <Banner image={bannerHome} />
-      <Container>
-        {
-          /* Lógica para agrupar os vídeos por categoria */
-          Array.from(new Set(videos.map((item) => item.category))).map((category) => (
-            <Category key={category} category={category}>
-              {videos
-                .filter((video) => video.category === category)
-                .map((item) => (
-                  <Card key={item.id} id={item.id} />
-                ))}
-            </Category>
-          ))
-        }
-      </Container>
-      <Footer />
-    </div>
+    <Home />
   );
 }
 
---
+++ src/pages/Home/index.js
@@ -0,0 +1,35 @@
+import Banner from "../../components/Banner";
+import Card from "../../components/Card";
+import Category from "../../components/Category";
+import Container from "../../components/Container";
+import Footer from "../../components/Footer";
+import Header from "../../components/Header";
+import videos from "../../json/videos.json";
+
+import bannerHome from "../../components/Banner/images/banner-home.png";
+
+function Home() {
+  return (
+    <div>
+      <Header />
+      <Banner image={bannerHome} />
+      <Container>
+        {
+          /* Lógica para agrupar os vídeos por categoria */
+          Array.from(new Set(videos.map((item) => item.category))).map((category) => (
+            <Category key={category} category={category}>
+              {videos
+                .filter((video) => video.category === category)
+                .map((item) => (
+                  <Card key={item.id} id={item.id} />
+                ))}
+            </Category>
+          ))
+        }
+      </Container>
+      <Footer />
+    </div>
+  );
+}
+
+export default Home;
