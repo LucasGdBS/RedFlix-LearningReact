bf0a95fb6697ff228cd5186ed05e51bd7a7678d3
Lucas-PC <lgbs@cesar.school>
2024-06-25 16:37:39 -0300

    import all images on app

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import Banner from "./components/Banner";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Container from "./components/Container";
-import homeImagen from "./components/Banner/images/banner-assistir.png"
+import bannerHome from "./components/Banner/images/banner-home.png"
+import bannerAssistir from "./components/Banner/images/banner-assistir.png"
+import bannerFavoritos from "./components/Banner/images/banner-favoritos.png"
 
 function App() {
   return (
     <div>
       <Header />
-      <Banner image={homeImagen}/>
+      <Banner image={bannerHome}/>
       <Container>
         <h1>Hello Word!</h1>
         <p>Dale familia</p>
