b0fee81403c013416728c34aa6334525fc414b7a
Lucas-PC <lgbs@cesar.school>
2024-06-24 22:39:02 -0300

    create header and footer components

---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import style from './Footer.module.css'
+
+function Footer(){
+    return (
+        <footer className={style.footer}>
+            <h2>RedFlix © Created by LucasGdBS 2024</h2>
+        </footer>
+    )
+}
+
+export default Footer;
