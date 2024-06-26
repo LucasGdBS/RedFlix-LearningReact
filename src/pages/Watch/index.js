a72c5a2c134ebebc4443b0e2f0e2c73180a8f78e
Lucas-PC <lgbs@cesar.school>
2024-06-25 23:22:42 -0300

    feat: adding dinamic pages for watch route

--- src/pages/Watch/index.js
+++ src/pages/Watch/index.js
@@ -2,8 +2,15 @@ import styles from "./Watch.module.css";
 import Header from "../../components/Header";
 import Container from "../../components/Container";
 import Footer from "../../components/Footer";
+import { useParams } from "react-router-dom";
+import videos from "../../json/videos.json";
+import PageNotFound from "../PageNotFound";
 
 function Watch() {
+  const params = useParams();
+  const video = videos.find((video) => video.id === params.id);
+  if (!video) return (<PageNotFound />)
+
   return (
     <>
       <Header />
@@ -12,7 +19,7 @@ function Watch() {
           <iframe
             width="854"
             height="480"
-            src="https://www.youtube.com/embed/2y1qW_aH0gQ?si=KQA_Brj7RgBWMZwd"
+            src={`https://www.youtube.com/embed/${video.id}`}
             title="YouTube video player"
             frameborder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
