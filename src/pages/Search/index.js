c3d49adefd74cbe5019f1a87ecadab1c75120747
Lucas-PC <lgbs@cesar.school>
2024-06-26 01:15:17 -0300

    create Search page

---
+++ src/pages/Search/index.js
@@ -0,0 +1,20 @@
+import styles from "./Search.module.css";
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
+import Container from "../../components/Container";
+
+function Search() {
+  return (
+    <>
+      <Header />
+      <Container>
+        <section className={styles.search}>
+          <h2>Pesquisar</h2>
+        </section>
+      </Container>
+      <Footer />
+    </>
+  );
+}
+
+export default Search;
