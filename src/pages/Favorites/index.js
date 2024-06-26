b2a379bbd8898533a2febdf54732fc6171c62b7c
Lucas-PC <lgbs@cesar.school>
2024-06-26 13:02:12 -0300

    implements VideoList on favorite pages

--- src/pages/Favorites/index.js
+++ src/pages/Favorites/index.js
@@ -2,15 +2,20 @@ import styles from "./Favorites.module.css";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 import Container from "../../components/Container";
+import VideoList from "../../components/VideoList";
+import { useFavoriteContext } from "../../contexts/Favorites";
 
 function Favorites() {
+
+  const {favorite} = useFavoriteContext();
+
   return (
     <>
       <Header />
       <Container>
         <section className={styles.favorites}>
           <h2 className={styles.title}>Meus Favoritss</h2>
-          Lista de videos favoritos
+          {<VideoList videos={favorite} emptyHeading="Nenhum vídeo favoritado ainda"/>}
         </section>
       </Container>
       <Footer />
