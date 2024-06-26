962cc3ba1f06dc09d8effd65fb8ceb4221f0f80c
Lucas-PC <lgbs@cesar.school>
2024-06-25 21:00:25 -0300

    refactor: adding links to header component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,14 +1,17 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 
+import { Link } from "react-router-dom";
 import styles from "./Header.module.css";
 
 function Header() {
   return (
     <header className={styles.header}>
-      <span>RedFlix</span>
+      <Link to="/">
+        <span>RedFlix</span>
+      </Link>
       <nav>
-        <a href="#">Home</a>
-        <a href="#">Assistir</a>
+        <Link to="/">Home</Link>
+        <Link to="/watch">Assistir</Link>
       </nav>
     </header>
   );
